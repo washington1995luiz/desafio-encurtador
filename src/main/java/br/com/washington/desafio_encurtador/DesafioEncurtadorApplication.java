ce0f9e0ace26f723bda57267bbe82dcf8cce7567
Washington <washington195luiz@gmail.com>
2024-09-04 00:59:49 -0300

    First commit

---
+++ src/main/java/br/com/washington/desafio_encurtador/DesafioEncurtadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.washington.desafio_encurtador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEncurtadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEncurtadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/washington/desafio_encurtador/DesafioEncurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.washington.desafio_encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEncurtadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
